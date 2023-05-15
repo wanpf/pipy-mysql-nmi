4f7cb64d5e414f3bc0033a897e2576b7ece77997
wanpf <wanpf@qq.com>
2023-05-15 16:31:13 +0800

    centos7环境静态编译

--- mak.sh
+++ mak.sh
@@ -1,3 +1,3 @@
 
-clang -I/var/root/cncf/wpf/pipy/include -I/var/root/cncf/wpf/pipy/src -I/usr/include/mysql /var/root/cncf/wpf/pipy/samples/nmi/pipy-mysql-nmi//mysql.c -L/usr/lib/x86_64-linux-gnu -l mysqlclient -lssl -lcrypto -lpthread -lstdc++ -ldl -fPIC -shared -o /var/root/cncf/wpf/pipy/samples/nmi/pipy-mysql-nmi//mysql-nmi.so
+gcc -I/root/pipy/pipy/include -I/root/pipy/pipy/src -I/usr/include/mysql /root/pipy/pipy/samples/nmi/pipy-mysql-nmi/mysql.c libmysqlclient.a libssl.a libcrypto.a -lstdc++ -lpthread -lm -ldl -fPIC -shared -o /root/pipy/pipy/samples/nmi/pipy-mysql-nmi/mysql-nmi.so 
 
